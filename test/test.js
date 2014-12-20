c650fd95b8250941928fc7c2484d2ea0442a14e9
Malte-Maurice Dreyer <dreyer.maltem@gmail.com>
2014-12-20 20:14:16 +0000

    Add tests

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var traceur = require("traceur");
+
+traceur.require.makeDefault(function(filename) {
+    // don't transpile our dependencies, just our app
+    return filename.indexOf("node_modules") === -1;
+});
+
+["flatten-array"].map(function(file) {
+    return "./" + file + ".es6";
+}).forEach(require);
